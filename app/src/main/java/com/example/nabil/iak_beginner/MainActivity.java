6a3529969148a86ff8a255cc346e603505a778e4
nabilrifm <nabilrifm@gmail.com>
2018-01-28 16:20:11 +0700

    Edit Activity

--- app/src/main/java/com/example/nabil/iak_beginner/MainActivity.java
+++ app/src/main/java/com/example/nabil/iak_beginner/MainActivity.java
@@ -3,10 +3,24 @@ package com.example.nabil.iak_beginner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = MainActivity.class.getSimpleName();
+
+    public final static String USERNAME = "username";
+    public final static String PASSWORD = "password";
+
+    private TextView tv_username;
+    private TextView tv_password;
+
+    String username_intent = getIntent().getStringExtra(USERNAME);
+    String password_intent = getIntent().getStringExtra(PASSWORD);
+
+    tv_username.SetText(username_intent);
+    tv_password.SetText(password_intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
